c7f6ef370ca89b5ceaa21184b96c5c304d7dba22
Colden Cullen <colden@coldencullen.com>
2025-09-11 01:12:43 +0000

    docs: add executor package docs

---
+++ pkg/executor/doc.go
@@ -0,0 +1,11 @@
+// Copyright © 2025 Colden Cullen
+// SPDX-License-Identifier: MIT
+
+// The executor package provides useful executors for building task-executing heirarchies.
+// Each package exports just a few objects conforming to the [go.bonk.build/pkg/task.GenericExecutor] interface,
+// and optionally a few helpers.
+//
+//   - [go.bonk.build/pkg/executor/tree]: allows building a tree of named executors for complex routing
+//   - [go.bonk.build/pkg/executor/rpc]: allows passing task invocations across an RPC boundary
+//   - [go.bonk.build/pkg/executor/plugin]: uses [github.com/hashicorp/go-plugin] to launch gRPC-based sub-processes
+package executor
