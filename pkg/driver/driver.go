24877bb253a451debbbc04f81a39577d5e348f55
Colden Cullen <colden@coldencullen.com>
2025-09-15 21:26:10 +0000

    test(drv): add tests for driver options

--- pkg/driver/driver.go
+++ pkg/driver/driver.go
@@ -5,14 +5,13 @@ package driver
 
 import (
 	"context"
-	"fmt"
-
-	"go.uber.org/multierr"
 
 	"go.bonk.build/pkg/executor/plugin"
 	"go.bonk.build/pkg/scheduler"
 	"go.bonk.build/pkg/task"
 )
+
+//go:generate go tool mockgen -destination driver_mock.go -package driver -copyright_file ../../license-header.txt -typed . Driver
 
 type Driver interface {
 	plugin.PluginClientManager
@@ -24,52 +23,4 @@ type Driver interface {
 	) (task.LocalSession, error)
 
 	Shutdown(ctx context.Context)
-}
-
-type DriverOption = func(context.Context, Driver) error
-
-func WithPlugins(plugins ...string) DriverOption {
-	return func(ctx context.Context, drv Driver) error {
-		return drv.StartPlugins(ctx, plugins...)
-	}
-}
-
-type SessionOption = func(context.Context, Driver, task.Session) error
-
-func WithLocalSession(path string, options ...SessionOption) DriverOption {
-	return func(ctx context.Context, drv Driver) error {
-		sess, err := drv.NewLocalSession(ctx, path)
-		if err != nil {
-			return err //nolint:wrapcheck
-		}
-
-		for _, option := range options {
-			multierr.AppendInto(&err, option(ctx, drv, sess))
-		}
-
-		if err != nil {
-			return fmt.Errorf("failed to initialize session with options: %w", err)
-		}
-
-		return nil
-	}
-}
-
-func WithTask[Params any](
-	executor, name string,
-	args Params,
-	inputs ...string,
-) SessionOption {
-	return func(ctx context.Context, drv Driver, session task.Session) error {
-		return drv.AddTask(
-			ctx,
-			task.New(
-				session,
-				executor,
-				name,
-				args,
-				inputs...,
-			).Box(),
-		)
-	}
 }
---
+++ pkg/driver/options.go
@@ -0,0 +1,61 @@
+// Copyright © 2025 Colden Cullen
+// SPDX-License-Identifier: MIT
+
+package driver
+
+import (
+	"context"
+	"fmt"
+
+	"go.uber.org/multierr"
+
+	"go.bonk.build/pkg/task"
+)
+
+type DriverOption = func(context.Context, Driver) error
+
+func WithPlugins(plugins ...string) DriverOption {
+	return func(ctx context.Context, drv Driver) error {
+		return drv.StartPlugins(ctx, plugins...)
+	}
+}
+
+type SessionOption = func(context.Context, Driver, task.Session) error
+
+func WithLocalSession(path string, options ...SessionOption) DriverOption {
+	return func(ctx context.Context, drv Driver) error {
+		sess, err := drv.NewLocalSession(ctx, path)
+		if err != nil {
+			return err //nolint:wrapcheck
+		}
+
+		for _, option := range options {
+			multierr.AppendInto(&err, option(ctx, drv, sess))
+		}
+
+		if err != nil {
+			return fmt.Errorf("failed to initialize session with options: %w", err)
+		}
+
+		return nil
+	}
+}
+
+func WithTask[Params any](
+	executor, name string,
+	args Params,
+	inputs ...string,
+) SessionOption {
+	return func(ctx context.Context, drv Driver, session task.Session) error {
+		return drv.AddTask(
+			ctx,
+			task.New(
+				session,
+				executor,
+				name,
+				args,
+				inputs...,
+			).Box(),
+		)
+	}
+}
