f632110a7dda0c3a047a7a5c323ece97073e8e89
Colden Cullen <colden@coldencullen.com>
2025-09-29 22:04:25 +0000

    feat(scheduler): implement rudimentary scheduler as an executor

--- go.mod
+++ go.mod
@@ -36,6 +36,7 @@ require (
 	go.uber.org/mock v0.6.0
 	go.uber.org/multierr v1.11.0
 	go.yaml.in/yaml/v4 v4.0.0-rc.2
+	golang.org/x/sync v0.17.0
 	google.golang.org/grpc v1.75.1
 	google.golang.org/protobuf v1.36.9
 	sigs.k8s.io/kustomize/api v0.20.1
@@ -186,7 +187,6 @@ require (
 	golang.org/x/mod v0.28.0 // indirect
 	golang.org/x/net v0.44.0 // indirect
 	golang.org/x/oauth2 v0.31.0 // indirect
-	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
 	golang.org/x/term v0.35.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
---
+++ pkg/executor/scheduler/scheduler_test.go
@@ -0,0 +1,65 @@
+// Copyright © 2025 Colden Cullen
+// SPDX-License-Identifier: MIT
+
+package scheduler_test
+
+import (
+	"context"
+	"strconv"
+	"testing"
+
+	"go.uber.org/mock/gomock"
+
+	"github.com/stretchr/testify/require"
+
+	"go.bonk.build/pkg/executor/scheduler"
+	"go.bonk.build/pkg/task"
+)
+
+func TestFollowups(t *testing.T) {
+	t.Parallel()
+
+	const numFollowups = 3
+
+	mock := gomock.NewController(t)
+	exec := task.NewMockExecutor(mock)
+	session := task.NewTestSession()
+
+	sched := scheduler.New(exec)
+
+	exec.EXPECT().OpenSession(t.Context(), session).Times(1)
+	exec.EXPECT().CloseSession(t.Context(), session.ID()).Times(1)
+
+	err := sched.OpenSession(t.Context(), session)
+	require.NoError(t, err)
+	defer sched.CloseSession(t.Context(), session.ID())
+
+	res := task.Result{}
+	tsk := task.New(
+		task.NewID("testing"),
+		session,
+		"none",
+		nil,
+	)
+
+	exec.EXPECT().
+		Execute(gomock.Any(), tsk, &res).
+		Times(1).
+		Return(nil).
+		Do(func(ctx context.Context, t *task.Task, r *task.Result) {
+			for idx := range numFollowups {
+				r.FollowupTasks = append(r.FollowupTasks, *task.New(
+					task.NewID("child", strconv.Itoa(idx)),
+					t.Session,
+					"none",
+					nil,
+				))
+			}
+		})
+	exec.EXPECT().
+		Execute(gomock.Any(), gomock.Any(), gomock.Any()).
+		Times(numFollowups)
+
+	err = sched.Execute(t.Context(), tsk, &res)
+	require.NoError(t, err)
+}
