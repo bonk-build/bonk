532414033685dde76500a8d041904d7b3f0c06ad
Colden Cullen <colden@coldencullen.com>
2025-09-29 23:29:45 +0000

    docs(exec): simplify executor package docs

--- pkg/executor/doc.go
+++ pkg/executor/doc.go
@@ -2,11 +2,10 @@
 // SPDX-License-Identifier: MIT
 
 // Package executor provides useful executors for building task-executing heirarchies.
-// Each package exports just a few objects conforming to the [go.bonk.build/pkg/task.Executor] interface,
+// Each package exports just a few objects conforming to the [task.Executor] interface,
 // and optionally a few helpers.
-//
-//   - [go.bonk.build/pkg/executor/statecheck]: allows using task states to avoid re-executing up-to-date tasks
-//   - [go.bonk.build/pkg/executor/tree]: allows building a tree of named executors for complex routing
-//   - [go.bonk.build/pkg/executor/rpc]: allows passing task invocations across an RPC boundary
-//   - [go.bonk.build/pkg/executor/plugin]: uses [github.com/hashicorp/go-plugin] to launch gRPC-based sub-processes
 package executor
+
+import (
+	_ "go.bonk.build/pkg/task"
+)
