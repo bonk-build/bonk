56ed7038be27c371b70e89518b8c31a9b197e963
Colden Cullen <colden@coldencullen.com>
2025-10-03 20:14:04 +0000

    test(scheduler): add test for dependency ordering

--- pkg/executor/scheduler/scheduler_test.go
+++ pkg/executor/scheduler/scheduler_test.go
@@ -156,3 +156,64 @@ func TestFollowupsErrs(t *testing.T) {
 	err = sched.Execute(t.Context(), tsk, &res)
 	require.ErrorIs(t, err, assert.AnError)
 }
+
+func TestDependenciesOrdering(t *testing.T) {
+	t.Parallel()
+
+	exec := mockexec.New(t)
+	session := task.NewTestSession()
+
+	sched := scheduler.New(exec, scheduler.NoConcurrencyLimit)
+
+	exec.EXPECT().OpenSession(t.Context(), session).Times(1)
+	exec.EXPECT().CloseSession(t.Context(), session.ID()).Times(1)
+
+	err := sched.OpenSession(t.Context(), session)
+	require.NoError(t, err)
+	defer sched.CloseSession(t.Context(), session.ID())
+
+	res := task.Result{}
+	tsk := task.New(
+		task.NewID("testing"),
+		session,
+		"none",
+		nil,
+	)
+
+	exec.EXPECT().
+		Execute(gomock.Any(), tsk, &res).
+		Times(1).
+		Return(nil).
+		Do(func(ctx context.Context, tsk *task.Task, res *task.Result) {
+			for idx := range 3 {
+				options := []task.Option{}
+				if idx != 2 {
+					options = append(options, task.WithDependencies(tsk.ID.GetChild("child", "2")))
+				}
+
+				res.FollowupTasks = append(res.FollowupTasks, *task.New(
+					task.NewID("child", strconv.Itoa(idx)),
+					tsk.Session,
+					"none",
+					nil,
+					options...,
+				))
+			}
+		})
+
+	child2 := exec.EXPECT().
+		Execute(gomock.Any(), task.TaskIDMatches(tsk.ID.GetChild("child", "2")), gomock.Any()).
+		Times(1)
+
+	exec.EXPECT().
+		Execute(gomock.Any(), task.TaskIDMatches(tsk.ID.GetChild("child", "0")), gomock.Any()).
+		Times(1).
+		After(child2)
+	exec.EXPECT().
+		Execute(gomock.Any(), task.TaskIDMatches(tsk.ID.GetChild("child", "1")), gomock.Any()).
+		Times(1).
+		After(child2)
+
+	err = sched.Execute(t.Context(), tsk, &res)
+	require.NoError(t, err)
+}
